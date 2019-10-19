9b0cf6a39507e96564b26d0997eea85aa0b3f9b2
takuro0987 <dfre0821@gmail.com>
2019-10-19 08:06:49 +0000

    comment機能

--- pepper/app/controllers/comments_controller.rb
+++ pepper/app/controllers/comments_controller.rb
@@ -1,7 +1,18 @@
 class CommentsController < ApplicationController
   def create
+    post = Post.find(params[:post_id])
+    comment = current_customer.comments.new(comment_params)
+    comment.post_id = post.id
+    comment.save
+    redirect_to root_path
   end
 
+
   def destroy
+  end
+
+  private
+  def comment_params
+    require.params(:comment).permit(:post_id, :comment)
   end
 end
