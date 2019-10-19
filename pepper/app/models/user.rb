26dd8f9c3543fa6c4e28c739aa9ac67b7c2742ff
19980410 <kkaoru1998@gmail.com>
2019-10-19 17:33:25 +0000

    post line

--- pepper/app/models/user.rb
+++ pepper/app/models/user.rb
@@ -2,21 +2,14 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-<<<<<<< HEAD
-         :recoverable, :rememberable, :validatable
-=======
           :recoverable, :rememberable, :validatable
->>>>>>> 6afaa11e71654d0f7a8f431a8f5a2b7b077c422f
   def email_required?
     false
   end
   def email_changed?
     false
   end
-<<<<<<< HEAD
-=======
 
   has_many :comments, dependent: :destroy
   has_many :posts,dependent: :destroy
->>>>>>> 6afaa11e71654d0f7a8f431a8f5a2b7b077c422f
 end
