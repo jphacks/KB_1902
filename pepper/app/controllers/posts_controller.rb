81eba102f479c096e9b50cc9f0f49de78f6ec4b6
19980410 <kkaoru1998@gmail.com>
2019-10-19 07:12:02 +0000

    投稿の完成

--- pepper/app/controllers/posts_controller.rb
+++ pepper/app/controllers/posts_controller.rb
@@ -1,11 +1,21 @@
 class PostsController < ApplicationController
   def index
+    @post = Post.new
   end
 
   def show
   end
 
   def create
+
+    @post = Post.new(post_params)
+    @post.genre_id = 1
+    @post.choice = params[:post][:choice]
+    if @post.save!
+      redirect_to post_path(@post.id)
+    else
+      render "index"
+    end
   end
 
   def destroy
@@ -18,5 +28,10 @@ class PostsController < ApplicationController
   end
 
   def new
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body ,:choice)
   end
 end
--- pepper/config/routes.rb
+++ pepper/config/routes.rb
@@ -1,16 +1,6 @@
 Rails.application.routes.draw do
-  get 'genre/create'
-  get 'genre/new'
-  get 'genre/destroy'
-  get 'posts/index'
-  get 'posts/show'
-  get 'posts/create'
-  get 'posts/destroy'
-  get 'posts/update'
-  get 'posts/edit'
-  get 'posts/new'
-  get 'comments/create'
-  get 'comments/destroy'
-  get 'comments/resources'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :posts
+  resources :comments
+  resources :genre
+
 end
