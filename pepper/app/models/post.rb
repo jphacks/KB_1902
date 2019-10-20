cfb277402d7f3fdeb13b87cf2ce8ff9fbffdc668
takuro0987 <dfre0821@gmail.com>
2019-10-20 04:01:29 +0000

    [add] タスク一覧

--- pepper/app/models/post.rb
+++ pepper/app/models/post.rb
@@ -1,6 +1,10 @@
 class Post < ApplicationRecord
     belongs_to :genre
     has_many :comments, dependent: :destroy
+    has_many :favos, dependent: :destroy
     belongs_to :user
+    def favorited_by?(user)
+        favos.where(user_id: user.id).exists?
+    end
 end
 
--- pepper/config/routes.rb
+++ pepper/config/routes.rb
@@ -4,12 +4,14 @@ Rails.application.routes.draw do
         registrations: 'users/registrations'
       }
   resources :users
+  resources :favos, only: [:index]
   post "/callback" => "posts#callback"
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "posts/search" => "posts#search"
   resources :posts do
   resources :comments
+  resource :favos, only: [:create, :destroy]
   end
   resources :genres
 
